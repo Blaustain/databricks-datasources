491c67912508082585c0c9e59c0ec8149c704762
Samuel Souza <jesusadonias1986@gmail.com>
2026-08-03 16:50:29 +0000

    databricks-datasources

--- adventure_work_dbt/models/marts/facts/fct_venda.sql
+++ adventure_work_dbt/models/marts/facts/fct_venda.sql
@@ -54,7 +54,7 @@ sales_reasons as (
 
     select
 
-        sr.sales_order_id,
+        sr.sales_order_id_sk,
 
         concat_ws(
             ', ',
@@ -65,7 +65,7 @@ sales_reasons as (
 
     inner join {{ ref('dim_motivo_venda') }} r
 
-        on sr.sales_reason_id = r.sales_reason_id
+        on sr.sales_reason_id_sk = r.sales_reason_id_sk
 
     group by sr.sales_order_id
 
